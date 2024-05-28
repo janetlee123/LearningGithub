d065390c1d2f73eb7d2576cff7b5974752080386
janetlee123 <84257686+janetlee123@users.noreply.github.com>
2024-05-28 10:53:05 -0400

    first python file

---
+++ firstpost.py
@@ -0,0 +1,22 @@
+#This my my first python code post 
+#Here I am just learning about strings 
+
+
+# Strings 
+intro = "Hi, my name is Janet"
+
+#Slicing 
+#slicing for the index 0 
+print(intro[0])
+
+#slicing from index 7-end of string 
+print(intro[7:])
+
+#len of string 
+print(len(intro))
+
+#Formatting 
+format='My number is: '+ str(5)
+print(format)
+
+
